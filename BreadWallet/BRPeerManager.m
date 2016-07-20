73954e540582bc130223127596922a8f738feca5
Quantum Explorer <quantumexplorer@hotmail.com>
2016-07-20 12:05:20 +0800

    testnet seeds

--- BreadWallet/BRPeerManager.m
+++ BreadWallet/BRPeerManager.m
@@ -57,7 +57,7 @@ static const struct { uint32_t height; const char *hash; uint32_t timestamp; uin
 };
 
 static const char *dns_seeds[] = {
-
+    "testnet1.groestlcoin.org.", "testnet2.groestlcoin.org."
 };
 
 #else // main net
